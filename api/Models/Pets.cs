9639358c667784931c3974c4be9c83eba7afc07f
Parker Randolph <pbrandolph@Parkers-MacBook-Air.local>
2024-04-16 13:55:51 -0500

    josh changes

--- api/Models/Pets.cs
+++ api/Models/Pets.cs
@@ -6,6 +6,7 @@ namespace api.Models
     {
         public int PetProfileId { get; set; }
         public int Age { get; set; }
+        public DateTime BirthDate { get; set; } 
         public string Breed { get; set; }
         public string Name { get; set; }
         public bool Availability { get; set; }
@@ -13,6 +14,9 @@ namespace api.Models
         public bool CanVisit { get; set; }
         public int ReturnedCount { get; set; }
         public int FavoriteCount { get; set; }
+        public bool favorited { get; set; }
+        public bool deleted { get; set; }
+        public int ShelterId { get; set; }
 
         public static List<Pets> GetAllPets() // method to retrieve pet from database
         {
@@ -29,16 +33,21 @@ namespace api.Models
                 myPets.Add(new Pets() // create pet object for each row
                 {
                     PetProfileId = rdr.GetInt32("PetProfileId"),
-                    Age = rdr.GetInt32("Age"),
                     Breed = rdr.GetString("Breed"),
                     Name = rdr.GetString("Name"),
+                    BirthDate = rdr.GetDateTime("BirthDate"),
                     Availability = rdr.GetInt32("Availability") == 1, // convert tinyint to boolean, if 1 then it is true
                     Species = rdr.GetString("Species"),
                     CanVisit = rdr.GetInt32("CanVisit") == 1,
                     ReturnedCount = rdr.GetInt32("ReturnedCount"),
-                    FavoriteCount = rdr.GetInt32("FavoriteCount")
+                    FavoriteCount = rdr.GetInt32("FavoriteCount"),
+                    ShelterId = rdr.GetInt32("ShelterId"),
+                    favorited = rdr.GetBoolean("deleted"),
+                    deleted = rdr.GetBoolean("favorited"),
+                    Age = rdr.GetInt32("Age"),
                 });
             }
+            con.Close();
             return myPets; // return populated list
         }
 
@@ -86,6 +95,36 @@ namespace api.Models
             cmd.ExecuteNonQuery(); // execute sql command
         }
 
+        public void FavoritePet(Pets value) {
+            GetPublicConnection cs = new GetPublicConnection(); // create new instance of database
+            using var con = new MySqlConnection(cs.cs);
+            con.Open(); // open db connection
+
+            using var cmd = new MySqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "UPDATE Pet_Profile SET favorited = @favorited WHERE PetProfileId = @PetProfileId";
+            cmd.Parameters.AddWithValue("@PetProfileId", value.PetProfileId);
+            cmd.Parameters.AddWithValue("@favorited", value.favorited);
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public void DeletePet(Pets value) {
+            GetPublicConnection cs = new GetPublicConnection();
+            using var con = new MySqlConnection(cs.cs);
+            con.Open();
+
+            using var cmd = new MySqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "UPDATE Pet_Profile SET deleted = @deleted WHERE PetProfileId = @PetProfileId";
+            cmd.Parameters.AddWithValue("@PetProfileId", value.PetProfileId);
+            cmd.Parameters.AddWithValue("@deleted", value.deleted);
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
 
         public static Pets GetPetById(int PetProfileId) // method to retrieve specific pet
         {
@@ -101,14 +140,18 @@ namespace api.Models
                 return new Pets() // construct and initialize new pet object
                 {
                     PetProfileId = rdr.GetInt32("PetProfileId"),
-                    Age = rdr.GetInt32("Age"),
                     Breed = rdr.GetString("Breed"),
                     Name = rdr.GetString("Name"),
+                    BirthDate = rdr.GetDateTime("BirthDate"),
                     Availability = rdr.GetInt32("Availability") == 1, // convert tinyint to boolean, if 1 then it is true
                     Species = rdr.GetString("Species"),
                     CanVisit = rdr.GetInt32("CanVisit") == 1,
                     ReturnedCount = rdr.GetInt32("ReturnedCount"),
-                    FavoriteCount = rdr.GetInt32("FavoriteCount")
+                    FavoriteCount = rdr.GetInt32("FavoriteCount"),
+                    ShelterId = rdr.GetInt32("ShelterId"),
+                    favorited = rdr.GetBoolean("deleted"),
+                    deleted = rdr.GetBoolean("favorited"),
+                    Age = rdr.GetInt32("Age"),
                 };
             }
             return null; // if no pet is found
