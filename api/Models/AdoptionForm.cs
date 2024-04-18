9116350738d9e78fcdd7617e642313f9077014f1
jblyon <jblyon@crimson.ua.edu>
2024-04-17 19:14:52 -0500

    josh 4/17 v6

--- api/Models/AdoptionForm.cs
+++ api/Models/AdoptionForm.cs
@@ -14,7 +14,6 @@ namespace api.Models
         public int PetProfileId { get; set; }
         public DateTime FormDate { get; set; }
         public bool Approved { get; set; }
-        public string FormNotes { get; set; }
         public bool deleted { get; set; }
 
         // Method to retrieve all adoption forms from the database
@@ -32,12 +31,12 @@ namespace api.Models
             {
                 forms.Add(new AdoptionForm()
                 {
-                    FormId = rdr.GetInt32("FormID"),
-                    UserId = rdr.GetInt32("UserID"),
-                    PetProfileId = rdr.GetInt32("PetProfileID"),
+                    FormId = rdr.GetInt32("FormId"),
+                    UserId = rdr.GetInt32("UserId"),
+                    PetProfileId = rdr.GetInt32("PetProfileId"),
                     FormDate = rdr.GetDateTime("FormDate"),
-                    Approved = rdr.GetInt32("Approved") == 1,
-                    FormNotes = rdr.GetString("FormNotes")
+                    Approved = rdr.GetBoolean("Approved"),
+                    deleted = rdr.GetBoolean("deleted"),
                 });
             }
             return forms;
@@ -46,23 +45,23 @@ namespace api.Models
         // Method to save the adoption form to the database
         public void SaveToDB()
         {
-            Data.GetPublicConnection cs = new Data.GetPublicConnection();
+            GetPublicConnection cs = new GetPublicConnection();
             using var con = new MySqlConnection(cs.cs);
             con.Open();
-            string stm = "INSERT INTO AdoptionForms (UserID, PetProfileID, FormDate, Approved, FormNotes) VALUES (@UserID, @PetProfileID, @FormDate, @Approved, @FormNotes)";
+            string stm = "INSERT INTO AdoptionForms (UserId, PetProfileId, FormDate, Approved, deleted) VALUES (@UserId, @PetProfileId, @FormDate, @Approved, @deleted)";
             using var cmd = new MySqlCommand(stm, con);
             cmd.Parameters.AddWithValue("@UserId", UserId);
             cmd.Parameters.AddWithValue("@PetProfileId", PetProfileId);
             cmd.Parameters.AddWithValue("@FormDate", FormDate);
-            cmd.Parameters.AddWithValue("@Approved", Approved ? 1 : 0);
-            cmd.Parameters.AddWithValue("@FormNotes", FormNotes);
+            cmd.Parameters.AddWithValue("@Approved", Approved);
+            cmd.Parameters.AddWithValue("@deleted", deleted);
             cmd.ExecuteNonQuery();
         }
 
         // Method to retrieve a specific adoption Form by ID
         public static AdoptionForm GetAdoptionFormById(int FormId)
         {
-            Data.GetPublicConnection cs = new Data.GetPublicConnection();
+            GetPublicConnection cs = new GetPublicConnection();
             using var con = new MySqlConnection(cs.cs);
             con.Open();
             string stm = "SELECT * FROM Adoption_Forms WHERE FormId = @FormId";
@@ -77,24 +76,25 @@ namespace api.Models
                     UserId = rdr.GetInt32("UserId"),
                     PetProfileId = rdr.GetInt32("PetProfileId"),
                     FormDate = rdr.GetDateTime("FormDate"),
-                    Approved = rdr.GetInt32("Approved") == 1,
-                    FormNotes = rdr.GetString("FormNotes")
+                    Approved = rdr.GetBoolean("Approved"),
+                    deleted = rdr.GetBoolean("deleted"),
                 };
             }
             return null;
         }
         public void UpdateToDB()
         {
-            Data.GetPublicConnection cs = new Data.GetPublicConnection();
+            GetPublicConnection cs = new GetPublicConnection();
             using var con = new MySqlConnection(cs.cs);
             con.Open();
-            string stm = "UPDATE Adoption_Forms SET FormId = @FormId, UserId = @UserId, PetProfileId = @PetProfileId, FormDate = @FormDate, Approved = @Approved, FormNotes = @FormNotes";
+            string stm = "UPDATE Adoption_Forms SET FormId = @FormId, UserId = @UserId, PetProfileId = @PetProfileId, FormDate = @FormDate, Approved = @Approved, deleted = @deleted";
             using var cmd = new MySqlCommand(stm, con);
             cmd.Parameters.AddWithValue("@FormId", FormId);
             cmd.Parameters.AddWithValue("@UserId", UserId);
             cmd.Parameters.AddWithValue("@FormDate", FormDate);
             cmd.Parameters.AddWithValue("@PetProfileId", PetProfileId);
             cmd.Parameters.AddWithValue("@Approved", Approved);
+            cmd.Parameters.AddWithValue("@deleted", deleted);
             cmd.ExecuteNonQuery();
         }
         public void DeleteAdoptionForm(AdoptionForm value) {
