4c9020193bf5a71622206f2c2a7f7e93758ed493
Greg <135546587+Greg4268@users.noreply.github.com>
2025-02-03 13:49:13 -0600

    upgrade to .NET9, adding .env, configuring for hosting again

--- api/Data/Database.cs
+++ api/Data/Database.cs
@@ -5,17 +5,6 @@ namespace api.Data
     {
         public string cs { get; set; }
 
-        // public GetPublicConnection()
-        // {
-        //     string server = "wftuqljwesiffol6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-        //     string port = "3306";
-        //     string username = "ra175v3sv9oai4bv";
-        //     string password = "v2xnjd2x4ctiwnqx";
-        //     string schema = "cve7x3dbdr8snfxd";
-
-        //     cs = $@"Server={server};Port={port};Database={schema};Uid={username};Pwd={password};";
-        // }
-
         public GetPublicConnection()
         {
             string server = "gtizpe105piw2gfq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
